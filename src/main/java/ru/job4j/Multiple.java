93b2a4c804f3d3de78bdd26656e3f5f54e8ca0a5
maxim <koba-87@mail.ru>
2022-08-05 20:05:48 +0300

    4.1. Разбор первой программы. [#159033] Multiple

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -14,6 +14,5 @@ public class Multiple {
         System.out.println("7*9=63");
         System.out.println("8*9=72");
         System.out.println("9*9=81");
-        System.out.println("9*10=90");:
     }
 }
