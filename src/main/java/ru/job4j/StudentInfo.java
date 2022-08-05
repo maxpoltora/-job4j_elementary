18fbcbe138d86fed7e268d511cf97c38acf4bc9a
maxim <koba-87@mail.ru>
2022-08-05 13:27:12 +0300

    Revert "3.3. Фиксация изменений на github [#243564 #328696]"
    This reverts commit 8ad100a5

--- src/main/java/ru/job4j/StudentInfo.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Poltorabatko Maxim");
-        System.out.println("28.02.1987");
-
-    }
-}
