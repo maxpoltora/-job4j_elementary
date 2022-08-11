6fde3e4be9ed75e4ffd1b5c3e4bfb9e16a4c1ec5
maxim <koba-87@mail.ru>
2022-08-11 13:04:23 +0300

    Update Converter

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -18,14 +18,6 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-//        float euro = Converter.rubleToEuro(140);
-//        float dollar = Converter.rubleToDollar(140);
-//        float ruEur = Converter.euroToRuble(140);
-//        float ruDoll = Converter.dollarToRuble(140);
-//        System.out.println("140 Rubles are " + euro + " euro.");
-//        System.out.println("140 Rubles are " + dollar + " dollar.");
-//        System.out.println("140 Euro are " + ruEur + " rubles.");
-//        System.out.println("140 Dollar are " + ruDoll + " rubles.");
         float in = 120;
         float expected = 2;
         float out = Converter.rubleToDollar(in);
