74c17d993e064b78c29df735f7985782dee738fc
maxim <koba-87@mail.ru>
2022-08-25 11:08:04 +0300

    6.6.0. Двумерный массив [#226796]

---
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class Sequence {
+
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+
+        array[0][0] = 12;
+        array[0][1] = 13;
+        array[0][2] = 14;
+
+        array[1][0] = 15;
+        array[1][1] = 16;
+        array[1][2] = 17;
+
+        array[2][0] = 18;
+        array[2][1] = 19;
+        array[2][2] = 10;
+
+    }
+
+}
